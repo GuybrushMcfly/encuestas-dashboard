900ab746d4988e7aec4cc4d7163b7dd72f8d61a8
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2026-01-21 09:11:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -249,7 +249,7 @@ wordcloud = WordCloud(
     background_color="white",
     max_words=40,
     stopwords=stopwords,
-    colormap="magma"
+    colormap="Blues"
 ).generate(texto)
 
 plt.figure(figsize=(12, 5))
